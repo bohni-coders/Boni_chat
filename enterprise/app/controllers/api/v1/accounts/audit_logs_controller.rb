296cf2751b315bb64899e45fa7d1cfd14dd7cfdb
bohnitech <68226460+bohnitech@users.noreply.github.com>
2024-01-22 22:45:56 +0530

    Audit logs latest

--- enterprise/app/controllers/api/v1/accounts/audit_logs_controller.rb
+++ enterprise/app/controllers/api/v1/accounts/audit_logs_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::Accounts::AuditLogsController < Api::V1::Accounts::EnterpriseAcco
   RESULTS_PER_PAGE = 15
 
   def show
-    @audit_logs = ::Enterprise::AuditLog.where(user_id: current_user.id).page(params[:page]).per(RESULTS_PER_PAGE)
+    @audit_logs = ::Enterprise::AuditLog.where(user_id: current_user.id).order(created_at: :desc).page(params[:page]).per(RESULTS_PER_PAGE)
     @current_page = @audit_logs.current_page
     @total_entries = @audit_logs.total_count
     @per_page = RESULTS_PER_PAGE
